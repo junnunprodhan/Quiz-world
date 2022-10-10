db68b3b1d54c70c227cddaada04e14afd0ba47ed
junnun15 <junnunprodhan@gmail.com>
2022-10-10 11:28:01 -0700

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Nav from './components/Nav/Nav';
+
 
 function App() {
   return (
     <div>
-      
+      <Nav></Nav>
     </div>
   );
 }
